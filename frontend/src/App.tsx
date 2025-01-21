d25b7773da47291e3ded61d0a7efb0b0f8545b7c
999Marv <marvinsiri123@gmail.com>
2025-01-21 11:19:56 -0500

    added react-router and tailwind for styling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,44 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import {
   SignedIn,
   SignedOut,
   SignInButton,
   UserButton,
 } from "@clerk/clerk-react";
+import Landing from "./Landing";
+import Home from "./Home";
+import Profile from "./Profile";
 
 export default function App() {
   return (
-    <header>
-      <SignedOut>
-        <SignInButton />
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-    </header>
+    <Router>
+      <header>
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </header>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route
+          path="/home"
+          element={
+            <SignedIn>
+              <Home />
+            </SignedIn>
+          }
+        />
+        <Route
+          path="/profile"
+          element={
+            <SignedIn>
+              <Profile />
+            </SignedIn>
+          }
+        />
+      </Routes>
+    </Router>
   );
 }
---
+++ frontend/src/Landing.tsx
@@ -0,0 +1,12 @@
+// import { useUser } from "@clerk/clerk-react";
+
+export default function Landing() {
+  //   const { user } = useUser();
+  return (
+    <>
+      <h1>Traveling soon?</h1>
+      <p>Taking forever to build that itinerary?</p>
+      <p></p>
+    </>
+  );
+}
